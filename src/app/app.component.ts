7492af13eb497de07a2223c3c18b9ece9d727cd9
Konstantin Kukhtinov <konstantin.kukhtinov@gmail.com>
2017-08-25 17:04:32 +0300

    7th try

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UserService} from './user.service';
+import {CookieService} from "angular2-cookie/core";
+import {user} from './user';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+    public user = new user();
+    public loadFlag = false;
+    protected token: string;
+
+    constructor(protected cooks: CookieService, protected userService: UserService) {
+    }
+
+    ngOnInit() {
+        const token = this.cooks.get('token');
+        if (token) {
+            this.user.token = token;
+            this.userService.loginByCookies(token).toPromise()
+                .then(response => response.json())
+                .then(response => {this.user.username = response.username.toString();
+                    this.user.status = response.status.toString();
+                })
+                .then(() => {
+                    this.userService.setSubjUser(this.user, 'root component');
+                    this.loadFlag = true;
+                })
+                .catch(err => console.log(err));
+        } else {
+            this.loadFlag = true;
+        }
+    }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,60 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {NavbarComponent} from './navbar.component';
+import {LoginFormComponent} from './login-form.component';
+import {HttpModule} from '@angular/http';
+import {IndexComponent} from "./index.component";
 
+import {NotesService} from './notes.service';
+import {AppRoutingModule} from './app-routing.module';
+import {NoteComponent} from "./note.component";
+import {SearchComponent} from './search.component';
+import {FormsModule} from "@angular/forms";
+import {UserService} from "./user.service";
+import {CookieService} from "angular2-cookie/core";
+import {RegisterComponent} from "./register.component";
+import {CommentsComponent} from "./comments.component";
+import {CommentsService} from "./comments.service";
+import {NoteCreateComponent} from "./note-create.component";
+import {UserComponent} from "./user.component";
+import {UsersAdminComponent} from "./users-admin.component";
+import {CommentsAdminComponent} from "./comments-admin.component";
+import {AdminComponent} from "./admin.component";
+import {NotesAdminComponent} from "./notes-admin.component";
+import {SearchService} from "./search.service";
+import {ProfileCreateComponent} from "./profile-create.component";
+import { CKEditorModule } from 'ng2-ckeditor';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NavbarComponent,
+        LoginFormComponent,
+        IndexComponent,
+        NoteComponent,
+        SearchComponent,
+        RegisterComponent,
+        CommentsComponent,
+        NoteCreateComponent,
+        UserComponent,
+        UsersAdminComponent,
+        CommentsAdminComponent,
+        AdminComponent,
+        NotesAdminComponent,
+        ProfileCreateComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        HttpModule,
+        CKEditorModule,
+    ],
+    providers: [NotesService, UserService, CookieService,
+        CommentsService,
+    SearchService],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
